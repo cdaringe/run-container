77924360b486dc7d413ac15ca33d258f0f55b445
cdaringe <christopher.dieringer@gmail.com>
2018-12-18 20:14:13 -0800

    fix(docs): restore typings reference

---
+++ docs/run-container.d.ts
@@ -0,0 +1,22 @@
+import Docker from 'dockerode';
+export declare function imageExists(imageName: string): Promise<boolean>;
+export interface RunContainerOptions extends Docker.ContainerCreateOptions {
+    Image: string;
+}
+export declare const run: (opts: RunContainerOptions) => Promise<Docker.Container>;
+export declare type RunSimpleOptions = {
+    autoRemove?: boolean;
+    bindMounts?: {
+        [hostPath: string]: string;
+    };
+    cmd?: Docker.ContainerCreateOptions['Cmd'];
+    env?: {
+        [key: string]: string;
+    };
+    image: string;
+    ports?: {
+        [containerTcpPort: string]: string;
+    };
+};
+export declare const runSimple: (opts: RunSimpleOptions) => Promise<Docker.Container>;
+//# sourceMappingURL=run-container.d.ts.map
