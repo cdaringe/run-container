b4a210e102344fd195da4d8ffb79e7f4487d8d4d
cdaringe <cdaringe@gmail.com>
2023-09-12 13:18:03 -0700

    chore: upgrade dockerode
    BREAKING CHANGE: dockerode intfcs have diverged

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,37 +1,39 @@
-import { runSimple } from '../'
-import Docker from 'dockerode'
-const docker = new Docker({ socketPath: '/var/run/docker.sock' })
-const demoContainerName = 'run-container-demo-pg'
+import { runSimple } from "../";
+import Docker from "dockerode";
+const docker = new Docker({ socketPath: "/var/run/docker.sock" });
+const demoContainerName = "run-container-demo-pg";
 
-async function go () {
-  await maybePruneContainer({ name: demoContainerName })
+async function go() {
+  await maybePruneContainer({ name: demoContainerName });
   const container = await runSimple({
     name: demoContainerName,
     autoRemove: true,
     verbose: true,
-    image: 'postgres',
+    image: "postgres",
     ports: {
-      '5432': '4000'
+      "5432": "4000",
     },
     env: {
-      POSTGRES_PASSWORD: `demo`
-    }
-  })
-  await new Promise(res => setTimeout(res, 1000))
-  const stats = await container.inspect()
-  console.log(stats)
-  await container.remove({ force: true })
+      POSTGRES_PASSWORD: `demo`,
+    },
+  });
+  await new Promise((res) => setTimeout(res, 1000));
+  const stats = await container.inspect();
+  console.log(stats);
+  await container.remove({ force: true });
 }
 
 async function maybePruneContainer({ name }: { name: string }) {
-  const containers =  await docker.listContainers({ all: true })
-  const existingContainerMeta = containers.find(container =>
-    container.Names.some(containerName => containerName.match(name))
-  )
+  const containers = await docker.listContainers({ all: true });
+  const existingContainerMeta = containers.find((container) =>
+    container.Names.some((containerName) => containerName.match(name))
+  );
   if (existingContainerMeta) {
-    const existingContainer = await docker.getContainer(existingContainerMeta.Id)
-    await existingContainer.remove({ force: true })
+    const existingContainer = await docker.getContainer(
+      existingContainerMeta.Id
+    );
+    await existingContainer.remove({ force: true });
   }
 }
 
-go()
+go();
--- src/__tests__/run.test.ts
+++ src/__tests__/run.test.ts
@@ -1,54 +1,62 @@
-import { run, runSimple } from '../run-container'
-import * as path from 'path'
-import ava, { TestInterface } from 'ava'
+import { run, runSimple } from "../run-container";
+import * as path from "path";
+import anyTest, { TestFn } from "ava";
 
-const test = ava as TestInterface<{}>
+const test = anyTest as TestFn<{ foo: string }>;
 
-const testBashScriptFilename = path.resolve(__dirname, 'slow-exit.sh')
+const testBashScriptFilename = path.resolve(__dirname, "slow-exit.sh");
 
-test('boots container', async t => {
+test("boots container", async (t) => {
   const container = await run({
-    Image: 'postgres',
+    Image: "postgres",
     ExposedPorts: {
-      '5432/tcp': {}
+      "5432/tcp": {},
     },
     HostConfig: {
       AutoRemove: true,
-      PortBindings: { '5432/tcp': [{ HostPort: '40000' }] }
-    }
-  })
-  const containerData = await container.inspect()
-  t.truthy(containerData.Image, 'has docker image')
-  t.is(containerData.State.Status, 'running', 'db is running')
+      PortBindings: { "5432/tcp": [{ HostPort: "40000" }] },
+    },
+  });
+  const containerData = await container.inspect();
+  t.truthy(containerData.Image, "has docker image");
+  t.is(containerData.State.Status, "running", "db is running");
   t.is(
     Object.keys(containerData.HostConfig.PortBindings).length,
     1,
-    'has host port exposed'
-  )
-  await container.remove({ force: true })
-})
+    "has host port exposed"
+  );
+  await container.remove({ force: true });
+});
 
-test('boots a simple container', async t => {
+test("boots a simple container", async (t) => {
   const container = await runSimple({
     autoRemove: false,
     bindMounts: {
-      [testBashScriptFilename]: '/test/test.sh'
+      [testBashScriptFilename]: "/test/test.sh",
     },
-    cmd: ['bash', '/test/test.sh'],
-    image: 'bash',
+    cmd: ["bash", "/test/test.sh"],
+    image: "bash",
     env: {
-      DB_URL: 'fake-path-to-db'
+      DB_URL: "fake-path-to-db",
     },
-    name: 'bashlyfe',
+    name: "bashlyfe",
     ports: {
-      4000: '4000'
-    }
-  })
-  const containerData = await container.inspect()
-  t.is(containerData.State.Status, 'running', 'db is running')
-  t.is(containerData.Name, '/bashlyfe')
-  await container.wait()
-  const stream = (await container.logs({ stdout: true })) as any
-  t.truthy((stream as string).match(/TESTING/))
-  await container.remove({ force: true })
-})
+      4000: "4000",
+    },
+  });
+  const containerData = await container.inspect();
+  t.is(containerData.State.Status, "running", "db is running");
+  t.is(containerData.Name, "/bashlyfe");
+  await container.wait();
+  const stream = await container.logs({ stdout: true });
+  const allLogParts: number[] = [];
+  for await (const v of stream) {
+    allLogParts.push(parseInt(v.toString()));
+  }
+  const fullLog = allLogParts
+    .map((c) => String.fromCharCode(c))
+    .join("")
+    .trim();
+  t.truthy(fullLog.match(/TESTING/));
+  await container.remove({ force: true });
+});
